b052dbc920d3fb0d4f9ced5a305e509e45ca0357
Sasho <aleksandar.e.angelov@gmail.com>
2013-10-06 18:32:26 +0300

    Elasticsearch plugin

--- elasticsearch.py
+++ elasticsearch.py
@@ -1,1 +1,26 @@
-AmonElasticSearchPlugin
+import requests
+import json
+
+from amonagent.settings import settings
+
+class AmonElasticsearchPlugin(object):
+
+	def __init__(self):
+		elasticsearch_config = settings.PLUGINS.get("elasticsearch", None)
+		status_url = "http://127.0.0.1:9200/_status" # Provide default
+		if elasticsearch_config:
+			status_url = elasticsearch_config.get("status_url", None)
+
+		self.url = status_url
+
+	def build_report(self):
+
+		status_dict = {}
+		response = requests.get(self.url)
+
+		if response.status_code == 200:		
+			status_dict = json.loads(response.text)
+		
+		return status_dict
+		
+plugin = AmonElasticsearchPlugin()
